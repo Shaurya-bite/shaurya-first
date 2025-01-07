f4572dec3cba3539e7409403a34eb331410ef86c
Shaurya-bite <vengalashaurya@gmail.com>
2025-01-07 20:48:41 +0530

    MADE MAJOR CHANGES

--- shaurya.py
+++ shaurya.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('first.html')
 
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0' , debug=True)
